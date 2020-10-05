cd41ab0adafce72e2bbe7842ac498d918d19152d
MuhammadFikrri <58206342+MuhammadFikrri@users.noreply.github.com>
2020-10-05 09:35:43 +0700

    TM2

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function main() {
   var vertexShaderCode = `
   void main() {
       gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-      gl_PointSize = 50.0;
+      gl_PointSize = 10.0;
   }
   `;
 
@@ -15,7 +15,7 @@ function main() {
 
   var fragmentShaderCode = `
   void main() {
-    gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
   }
   `;
 
@@ -29,7 +29,7 @@ function main() {
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
 
-  gl.clearColor(0.2, 0.0, 0.0, 1.0);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
   gl.drawArrays(gl.POINTS, 0, 1);
